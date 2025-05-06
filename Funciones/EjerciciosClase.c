318390c04214e1024d907803cff8e77cfe445c1b
Ethan Flores <ethan.sebas.los@gmail.com>
2025-05-06 15:25:54 -0500

    Se acabo ejercicio en clase

--- Funciones/EjerciciosClase.c
+++ Funciones/EjerciciosClase.c
@@ -6,11 +6,11 @@ float leerNumero(char* mensaje);
 
 int main()
 {
-    float Lado1,Lado2,hipo;
+    float Lado1,Lado2;
     Lado1 = leerNumero("Ingrese el lado 1: ");
     Lado2 = leerNumero("Ingrese el lado 2: ");
     printf("-----------------------------\n");
-    printf("La hipotenusa es %.2f",hipotenusa(Lado1,Lado2));
+    printf("La hipotenusa es %.2f\n",hipotenusa(Lado1,Lado2));
 }
 
 float hipotenusa(float Cat1 , float Cat2){
@@ -21,12 +21,12 @@ float hipotenusa(float Cat1 , float Cat2){
 
 float leerNumero(char* mensaje){
     float valor;
-    printf(" %s: ",mensaje);
-    while (scanf("%f",&valor) != 1)
+    printf("%s",mensaje);
+    while (scanf("%f",&valor) != 1 || valor < 0)
     {
         printf("Dato mal ingresado\n");
         while ((getchar()) != '\n'); 
-        printf("%s ",mensaje);
+        printf("%s",mensaje);
     }
     return valor;
 }
